703c9cff2ad486ff3b154f777f9570051f1e6d5b
sleeve5 <1244595156@qq.com>
2022-04-26 22:06:42 +0800

    commit

---
+++ 14_nn_loss.py
@@ -0,0 +1,22 @@
+# author: Yin Xinyu
+
+import torch
+from torch.nn import L1Loss
+
+inputs = torch.tensor([1, 2, 3], dtype=torch.float32)
+targets = torch.tensor([1, 2, 5], dtype=torch.float32)
+
+inputs = torch.reshape(inputs, (1, 1, 1, 3))
+targets = torch.reshape(targets, (1, 1, 1, 3))
+
+loss = L1Loss()
+result = loss(inputs, targets)
+
+print(result)
+
+x = torch.tensor([0.1, 0.2, 0.3])
+y = torch.tensor([1])
+x = torch.reshape(x, (1, 3))
+loss_cross = torch.nn.CrossEntropyLoss()
+result = loss_cross(x, y)
+print(result)
